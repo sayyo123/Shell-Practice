4ab3cd77150c630eb76192d5bdd5fa32c6f6a568
zjx0126 <719010493@qq.com>
2018-02-28 07:00:06 -0500

    script-05

---
+++ ping2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+	echo "usage /ping2.sh [network address]"
+	echo "example ./ping2.sh 172.16.33.0"
+	exit
+fi
+
+prefix=$(echo $1 | cut -d '.' -f 1-3)
+for addr in $(seq 100 254); do
+	hping3 $prefix.$addr --udp -c 1 >> temp.txt
+done
+grep Unreachable temp.txt | cut -d " " -f 5 | cut -d "=" -f 2 >>output.txt
+rm temp.txt
